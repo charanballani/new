cf57c777b528506947ff46acd7f9a4eb95337518
charan <charan123>
2026-02-05 08:00:45 +0000

    this is java file commit

---
+++ file1.java
@@ -0,0 +1,8 @@
+//this is a java sample code
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This line prints the specified text to the console
+        System.out.println("Hello, World!");
+    }
+}
+
